e98ab6e1590b630af66c381a0e8594607e519849
Raisa Tathoi <tathoi.raisa@gmail.com>
2024-08-15 14:52:13 -0600

    changed the QR code and emailing system

--- registration/models.py
+++ registration/models.py
@@ -9,19 +9,32 @@ class Booking(models.Model):
     school_email = models.EmailField()
     number_of_tickets = models.PositiveIntegerField()
     payment_due = models.FloatField(editable=False, default=0)
-    qr_code = models.ImageField(upload_to='qr_codes', blank=True, null=True)
+    payment_qr_code = models.ImageField(upload_to='payment_qr_codes', blank=True, null=True)
+    booking_qr_code = models.ImageField(upload_to='booking_qr_codes', blank=True, null=True)
     paid = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        if not self.qr_code:
+        # Generate payment QR code
+        if not self.payment_qr_code:
             qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-            qr.add_data(f'https://swc-prom-ticketing.onrender.com/booking/{self.id}')
+            qr.add_data(f'https://swc-prom-ticketing.onrender.com/payment_due/{self.id}')
             qr.make(fit=True)
             img = qr.make_image(fill='black', back_color='white')
             buffer = BytesIO()
             img.save(buffer, format='PNG')
-            file_name = f'qr_code_{self.id}.png'
-            self.qr_code.save(file_name, File(buffer), save=False)
+            file_name = f'payment_qr_code_{self.id}.png'
+            self.payment_qr_code.save(file_name, File(buffer), save=False)
+
+        # Generate booking QR code if paid
+        if self.paid and not self.booking_qr_code:
+            qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+            qr.add_data(f'https://swc-prom-ticketing.onrender.com/booking_details/{self.id}')
+            qr.make(fit=True)
+            img = qr.make_image(fill='black', back_color='white')
+            buffer = BytesIO()
+            img.save(buffer, format='PNG')
+            file_name = f'booking_qr_code_{self.id}.png'
+            self.booking_qr_code.save(file_name, File(buffer), save=False)
 
         if self.number_of_tickets % 2 == 0:
             pending = self.number_of_tickets * 5
--- registration/views.py
+++ registration/views.py
@@ -3,6 +3,10 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from .forms import BookingForm
 from .models import Booking, Guest
+import qrcode
+from io import BytesIO
+from django.core.files import File
+
 
 def book_tickets(request):
     if request.method == 'POST':
@@ -22,19 +26,21 @@ def book_tickets(request):
                     if send_copy_to_guests:
                         guest_emails.append(guest_email)
 
-            if send_copy_to_guests:
-                email_recipients = [booking.school_email] + guest_emails
-            else:
-                email_recipients = [booking.school_email]
+            # Generate QR code for payment_due
+            payment_qr_code = generate_qr_code(f'https://swc-prom-ticketing.onrender.com/payment_due/{booking.id}')
+            booking.payment_qr_code.save(f'payment_qr_code_{booking.id}.png', payment_qr_code, save=False)
+            booking.save()  # Save QR code path to model
 
-            email = EmailMessage(
-                'Your Booking QR Code',
-                'Please find your QR code attached.',
+            # Initial email with payment_due QR code
+            email_recipients = [booking.school_email] + guest_emails
+            initial_email = EmailMessage(
+                'Your Payment QR Code',
+                'The QR below, when scanned takes you to a page that displays the total amount that you have to pay for your tickets. Please show this at the booth when purchasing your tickets.',
                 settings.DEFAULT_FROM_EMAIL,
                 email_recipients
             )
-            email.attach_file(booking.qr_code.path)
-            email.send()
+            initial_email.attach_file(booking.payment_qr_code.path)
+            initial_email.send()
 
             return redirect('success')
         else:
@@ -45,11 +51,50 @@ def book_tickets(request):
 
     return render(request, 'booking/book_tickets.html', {'form': form})
 
+
+def mark_as_paid(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.paid = True
+    booking.save()
+
+    # Send booking details email
+    email_recipients = [booking.school_email] + [guest.email for guest in booking.guests.all()]
+    booking_details_email = EmailMessage(
+        'Your Booking Details',
+        'Please find your booking details QR code attached. Show this to the volunteer at the entrance to redeem your ticket. BE SURE TO BRING YOUR SCHOOL ID OR A PICTURE IDENTIFICATION WITH YOU!!',
+        settings.DEFAULT_FROM_EMAIL,
+        email_recipients
+    )
+
+    # Generate QR code for booking details
+    booking_qr_code = generate_qr_code(f'https://swc-prom-ticketing.onrender.com/booking_details/{booking.id}')
+    booking.booking_qr_code.save(f'booking_qr_code_{booking.id}.png', booking_qr_code, save=False)
+    booking.save()  # Save QR code path to model
+
+    booking_details_email.attach_file(booking.booking_qr_code.path)
+    booking_details_email.send()
+
+    return redirect('success')
+
+
+def generate_qr_code(url):
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    buffer = BytesIO()
+    img.save(buffer, format='PNG')
+    buffer.seek(0)
+    return File(buffer, name=f'qr_code_{url.split("/")[-1]}.png')
+
+
 def success(request):
     return render(request, 'booking/success.html')
 
+
 def index(request):
     return render(request, 'booking/index.html')
+
 
 def qr_code_scan(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
